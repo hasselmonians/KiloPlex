5466798ed940dd5dfe506666d6fc09e248022197
Alec Hoyland <alec.hoyland@posteo.net>
2019-03-26 11:57:42 -0400

    Actually build the file

--- readPLXFileC/plx2mat.m
+++ readPLXFileC/plx2mat.m
@@ -2,6 +2,7 @@ function plx = plx2mat(filename)
 
   if ~exist('readPLXFileC.mexa64', 'file')
     disp('[INFO] building ''readPLXFile.c''')
+    build_readPLXFileC()
   end
 
   plx = readPLXFileC(filename);
