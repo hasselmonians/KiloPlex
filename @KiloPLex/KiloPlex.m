a6707abd11a90da12e0c8632d6186cf1ae6ab690
Alec Hoyland <alec.hoyland@posteo.net>
2019-03-29 16:56:07 -0400

    Add basic structure of class

---
+++ @KiloPLex/KiloPlex.m
@@ -0,0 +1,19 @@
+classdef KiloPlex
+
+properties
+
+  options
+  % a struct that contains all the options required
+  % to run KiloSort2
+
+end % properties
+
+methods
+
+end % methods
+
+methods (Static)
+
+end % static methods
+
+end % classdef
