fb02befaa4407a5888dcf9f2e48e63f4c4ad929a
Alec Hoyland <alec.hoyland@posteo.net>
2019-04-17 14:09:28 -0400

    Add function to convert regular matrices to KiloSort compatible ones

---
+++ @KiloPlex/convert.m
@@ -0,0 +1,35 @@
+function data = convert(data, verbose)
+  % forces a matrix to contain data of type int16
+  % if the matrix is not int16, normalizes by the minimum value
+  % to force all values to be integers
+
+  % this is necessary for a data matrix to be passed to kilosort
+
+  if nargin < 2
+    verbose = false
+  end
+
+  w = whos('data');
+
+  if w.class == 'int16'
+    if verbose
+      disp('[INFO] data are already of class int16')
+    end
+  else
+    data = data ./ min(data(:));
+    if verbose
+      disp('[INFO] scaling data by min(data(:))')
+    end
+    if verbose
+      disp('[INFO] converting to int16')
+    end
+  end
+
+  if size(data, 1) > size(data, 2)
+    data = data';
+    if verbose
+      disp('[INFO] second dimension should be time...transposing data')
+    end
+  end
+
+end % function
