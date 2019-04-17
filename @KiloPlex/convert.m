2cc3d290d429b233cf27b8901cec75aaf648d1d6
Alec Hoyland <alec.hoyland@posteo.net>
2019-04-17 14:30:16 -0400

    Add documentation, save file, convert to int16

--- @KiloPlex/convert.m
+++ @KiloPlex/convert.m
@@ -1,11 +1,25 @@
-function data = convert(data, verbose)
+function data = convert(data, filepath, verbose)
   % forces a matrix to contain data of type int16
   % if the matrix is not int16, normalizes by the minimum value
   % to force all values to be integers
+  %
+  % Arguments:
+  %   data: a matrix of some numerical type (usually double)
+  %     the matrix should contain electrophysiology data
+  %     as time-series from electrodes
+  %   filepath: the full filepath (.bin file) where the data should be saved
+  %     if this is empty, no data are saved (kept in workspace)
+  %   verbose: a flag to toggle debugging display statements
+  %
+  %   myConvertedData = KiloPlex.convert(myData, 'path2file/filename.bin', true)
 
   % this is necessary for a data matrix to be passed to kilosort
 
   if nargin < 2
+    filepath = [];
+  end
+
+  if nargin < 3
     verbose = false
   end
 
@@ -20,6 +34,7 @@ function data = convert(data, verbose)
     if verbose
       disp('[INFO] scaling data by nonzero absolute minimum')
     end
+    data = int16(data);
     if verbose
       disp('[INFO] converting to int16')
     end
@@ -30,6 +45,13 @@ function data = convert(data, verbose)
     if verbose
       disp('[INFO] second dimension should be time...transposing data')
     end
+  end
+
+  if ~isempty(filepath)
+    fileID = fopen(filepath, 'w');
+    fwrite(fileID, data, 'int16');
+    fclose(fileID);
+    disp('[INFO] saved data')
   end
 
 end % function
