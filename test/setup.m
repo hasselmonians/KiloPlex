b164812b1220cb6a45098a5b71a29cb941c74f0e
Alec Hoyland <alec.hoyland@posteo.net>
2019-05-03 15:34:23 -0400

    Create options file, copy channel map

--- test/setup.m
+++ test/setup.m
@@ -1,5 +1,6 @@
 % setup script to create the required RatCatcher object and batch
 
+% generate the RatCatcher object
 r = RatCatcher;
 r.expID           = 'test';
 r.remotepath      = '/projectnb/hasselmogrp/hoyland/KiloPlex/cluster';
@@ -10,4 +11,13 @@ r.batchscriptpath = which('KiloPlex-batch-script.sh');
 r.project         = 'hasselmogrp';
 r.verbose         = true;
 
+% create the options file
+getOptions;
+r.lamplight(options);
+
+% copy the channel map file
+% downloaded (and renamed) from data.cortexlab.net
+copyfile(which('chanMap.mat'), r.localpath);
+
+% perform the batching process
 r                 = r.batchify();
